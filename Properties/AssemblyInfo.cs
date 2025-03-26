8956e5cf66b3ffe034143f7307e6891ff6ae358f
ZygoteCode <zygotecode@gmail.com>
2025-03-26 08:13:13 +0100

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GPSharp")]
-[assembly: AssemblyDescription("The first absolute understandable wrapper for Italian Green Pass, made by ZioEren.")]
+[assembly: AssemblyDescription("The first absolute understandable wrapper for Italian Green Pass, made by ZygoteCode.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZioEren")]
 [assembly: AssemblyProduct("GPSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2022, ZioEren. All rights reserved.")]
-[assembly: AssemblyTrademark("Copyright ©  2022, ZioEren. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright ©  2025, ZygoteCode. All rights reserved.")]
+[assembly: AssemblyTrademark("Copyright ©  2025, ZygoteCode. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c67bc8e8-9542-454d-8cb9-4b21cada656f")]
